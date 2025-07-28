3b98baa3a86aa97c0893b179eee85e188bcb8f2b
kadampratikengg <kadampratik.engg@gmail.com>
2025-07-28 16:21:25 +0530

    28072025

--- server/server.js
+++ server/server.js
@@ -14,6 +14,11 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
+app.use(cors({
+  origin: 'https://appointment-eight-zeta.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
 app.use(express.json());
 
 app.use('/api/appointments', appointmentRoutes);
